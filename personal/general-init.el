edc0f79c6575dd15b0de811a644b8fea169fe9b6
Luke Randall <luke.randall@gmail.com>
2013-04-28 21:44:14 +0200

    Remove smex config as its part of prelude

--- personal/general-init.el
+++ personal/general-init.el
@@ -3,10 +3,6 @@
 
 (electric-indent-mode 1)
 
-;; smex
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "C-x C-m") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ;; navigation & marks
