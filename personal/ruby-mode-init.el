894e3992cdd574de444b8e77696a75c065d70a47
Luke Randall <luke.randall@gmail.com>
2013-07-22 16:21:28 +0200

    Add robe-mode to ruby-mode-hook

--- personal/ruby-mode-init.el
+++ personal/ruby-mode-init.el
@@ -4,3 +4,11 @@
   (occur "^\\s-*\\\(class \\\|module \\\|def \\\|[^:]include \\\|private\\b\\\|protected\\b\\\)"))
 
 (define-key ruby-mode-map (kbd "C-c C-t") 'rgc-show-ruby-tags)
+
+(add-hook 'ruby-mode-hook 'robe-mode)
+
+;; - M-. to jump to the definition
+;; - M-, to jump back
+;; - C-c C-d to see the documentation
+;; - C-c C-k to refresh Rails environment
+;; - C-M-i to complete the symbol at point
