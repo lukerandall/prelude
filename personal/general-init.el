967cdc60c4f8643cb71941624978dbebda001597
Luke Randall <luke.randall@gmail.com>
2013-07-26 12:09:29 +0200

    Keybinding updates

--- personal/general-init.el
+++ personal/general-init.el
@@ -19,14 +19,15 @@
 (define-key global-map [?\s-e] 'er/expand-region)
 (define-key global-map [?\s-f] 'projectile-find-file)
 (define-key global-map [?\s-s] 'projectile-switch-project)
-(define-key global-map [?\s-g] 'projectile-grep)
+(define-key global-map [?\s-a] 'projectile-ack)
 (define-key global-map [?\s-j] 'prelude-top-join-line)
 (define-key global-map [?\s-k] 'prelude-kill-whole-line)
 (define-key global-map [?\s-l] 'goto-line)
 (define-key global-map [?\s-m] 'magit-status)
-(define-key global-map [?\s-w] 'delete-frame)
+(define-key global-map [?\s-w] 'delete-window)
 (define-key global-map [?\s-x] 'exchange-point-and-mark)
-(define-key global-map [?\s-p] 'projectile-switch-project)
+(define-key global-map [?\s-p] 'dash-at-point)
+(define-key global-map [?\s-r] 'split-window-right)
 
 ;; key chords
 (key-chord-define-global "jf" 'projectile-find-file)
