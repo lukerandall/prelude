6e0d0914f73560c4077b5e74326eef71ecb2589c
Luke Randall <luke.randall@gmail.com>
2014-05-14 09:12:30 +0200

    Use web-mode for heist templates

--- personal/web-mode-init.el
+++ personal/web-mode-init.el
@@ -7,3 +7,4 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
